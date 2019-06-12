ebfd61752ba669dd6afb39ce86d8aa7d4e39b3a0
Rwothoromo Elijah <rwothoromoelijah@gmail.com>
2019-06-12 22:03:19 +0300

    Update configs

--- config.py
+++ config.py
@@ -68,13 +68,13 @@ class Config:
 
     # Alternatively, use a local MySQL instance for testing.
     LOCAL_SQLALCHEMY_DATABASE_URI = (
-        'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{database}').format(
+        'postgresql://{user}:{password}@127.0.0.1:3306/{database}').format(
             user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
             database=CLOUDSQL_DATABASE)
 
     # When running on App Engine, a unix socket is used to connect to the cloudsql instance.
     LIVE_SQLALCHEMY_DATABASE_URI = (
-        'mysql+pymysql://{user}:{password}@localhost/{database}'
+        'postgresql://{user}:{password}@localhost/{database}'
         '?unix_socket=/cloudsql/{connection_name}').format(
             user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
             database=CLOUDSQL_DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME)
