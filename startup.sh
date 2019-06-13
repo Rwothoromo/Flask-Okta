a6e6fba7daad465e20d935b21d38f325af4ad323
Rwothoromo Elijah <rwothoromoelijah@gmail.com>
2019-06-13 18:48:11 +0300

    Update startup script

--- startup.sh
+++ startup.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 # Sets up env variables and database
 sudo su -
+sudo apt update
+sudo apt install python3-pip
+pip3 install virtualenv
+cd Flask-Okta
+virtualenv ../flask-okta-venv --python=python3
 pip install -r requirements.txt
 source .env
 python manage.py db init
